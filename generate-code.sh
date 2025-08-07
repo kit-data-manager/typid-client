9d8b51517c0bcd565da1080232b03e0a950948ab
Andreas Pfeil <andreas.pfeil.rs@gmail.com>
2025-08-07 13:06:11 +0200

    fix: correct path for codegen script

--- generate-code.sh
+++ generate-code.sh
@@ -3,4 +3,4 @@ echo ${PWD}
 podman run --rm -v ${PWD}:/local docker.io/openapitools/openapi-generator-cli:latest generate \
     -i https://typed-pid-maker.datamanager.kit.edu/preview/v3/api-docs \
     -g python \
-    -o /local/src/typid_client/generated
+    -o /local
