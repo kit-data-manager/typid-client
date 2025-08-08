890e99a611e2136cd6a2a8a5943118d5cb0e8337
Andreas Pfeil <andreas.pfeil@kit.edu>
2025-08-08 15:22:25 +0200

    test: batch creation api

--- test/test_pid_management_api.py
+++ test/test_pid_management_api.py
@@ -17,6 +17,7 @@ import unittest
 
 from openapi_client.api.pid_management_api import PIDManagementApi
 from openapi_client.highlevel import SimpleRecord
+from openapi_client.models.batch_record_response import BatchRecordResponse
 
 
 class TestPIDManagementApi(unittest.TestCase):
@@ -55,7 +56,45 @@ class TestPIDManagementApi(unittest.TestCase):
 
         Create a multiple, possibly related PID records
         """
-        pass
+        input1 = SimpleRecord.from_dict({
+            "pid": "a",
+            "record": [
+                { "key": "21.T11148/d0773859091aeb451528", "value": "b" }
+            ]
+        })
+
+        input2 = SimpleRecord.from_dict({
+            "pid": "b",
+            "record": [
+                { "key": "21.T11148/d0773859091aeb451528", "value": "a" }
+            ]
+        })
+
+        assert input1 is not None, "Input1 should not be None"
+        assert input2 is not None, "Input2 should not be None"
+
+        result: BatchRecordResponse = self.api.create_pids(
+            pid_record=[input1.to_record(), input2.to_record()]
+        )
+
+        assert result is not None, "Result should not be None"
+        assert result.pid_records is not None, "PID records should not be None"
+        assert len(result.pid_records) == 2, "There should be two PID records created"
+
+        a = result.pid_records[0]
+        b = result.pid_records[1]
+
+        assert a.entries is not None, "Entries for PID 'a' should not be None"
+        assert b.entries is not None, "Entries for PID 'b' should not be None"
+
+        assert len(a.entries) == 1, "PID 'a' should have one entry"
+        assert len(b.entries) == 1, "PID 'b' should have one entry"
+
+        assert a.entries["21.T11148/d0773859091aeb451528"][0].value == b.pid, \
+            "Value for PID 'a' should be 'b'"
+        assert b.entries["21.T11148/d0773859091aeb451528"][0].value == a.pid, \
+            "Value for PID 'b' should be 'a'"
+
 
     def test_find_all(self) -> None:
         """Test case for find_all
