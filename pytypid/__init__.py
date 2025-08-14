bb2c9c951310145bcde9795fb2afce76e5906849
Andreas Pfeil <andreas.pfeil.rs@gmail.com>
2025-08-14 12:33:51 +0200

    fix: missing explicit public members declaration

--- pytypid/__init__.py
+++ pytypid/__init__.py
@@ -1,3 +1,8 @@
 from .record import SimpleRecord
 from pytypid_generated_client.api import PIDManagementApi
 from pytypid_generated_client.models import BatchRecordResponse
+
+# Explicit public members
+__all__ = [
+    "SimpleRecord"
+]
